29e1ed69656df8378606476ceb5d88d633e533d4
Dev Reactor <48456727+dev-reactor@users.noreply.github.com>
2021-05-04 16:15:21 -0400

    update python

---
+++ .history/src/py-web-scrapers/web-scraper_20210504161501.py
@@ -0,0 +1,23 @@
+  
+import urllib2
+import csv
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+#webpage to scrape
+recipes = 'https://www.allrecipes.com/'
+
+#actual html of page
+page = urllib2.urlopen(recipes)
+
+#page parsed for bs4
+soup = BeautifulSoup(page, 'html.parser')
+
+recipe_containers = soup.find_all('h2', attrs={'class': 'recipe-nutrition-section'} )
+# Output to csv file
+for recipe_container in recipe_containers:
+    data = recipe_containers.text
+    if 'calories' in data:
+        with open('index.csv', 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([data])
